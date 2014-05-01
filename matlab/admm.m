b30bdd9fa635ffc36a680c2a7fa44a17d0c572b5
Chris F <chris.fougner@gmail.com>
2014-05-01 16:30:48 -0700

    Added lots of comments to admm.m

--- matlab/admm.m
+++ matlab/admm.m
@@ -1,12 +1,81 @@
 function [x12, factors] = admm(prox_f, prox_g, obj_fn, A, params, factors)
-% Generic graph projection splitting solver. Solve
+%%ADMM Generic graph projection splitting solver.
+%   Solves problems in the form
 %
-%  minimize    f(y) + g(x)
-%  subject to  y = Ax
+%     minimize    f(y) + g(x)
+%     subject to  y = Ax
 % 
-% given prox_f, prox_g, and A.
+%   where the proximal operators of the functions f and g are known.
+%
+%   [x12, factors] = admm(prox_f, prox_g, obj_fn, A)
+%   [x12, factors] = admm(prox_f, prox_g, obj_fn, A, params, factors)
+% 
+%   Optional Inputs: params, factors
+%
+%   Inputs
+%   prox_f    - Function handle or cell array of function handles, which
+%               calculates returns the proximal operator of the function f.
+%               If prox_f is a function handle, then it must accept vector
+%               arguments and if prox_f is a cell array, then it must have
+%               dimension equal to the number of rows in A. The function
+%               handles must accept input in the form prox_f(x_i, rho), 
+%               where rho is the penalty parameter.
+%
+%   prox_g    - Proximal operator for the function g. See description of
+%               prox_f, with only difference that the cell array must have
+%               dimension equal to the number of columns in A.
+%
+%   obj_fn    - Function handle which satsifies obj_fn(x, y) = f(y) + g(x).
+%
+%   A         - Complicating constraint matrix of dimension m * n.
+%
+%   Optional Input
+%   params    - Structure of parameters, containing any of the following
+%               fields:
+%                 + ABSTOL (default 1e-4): Absolute tolerance to which the
+%                   problem should be solved.
+%                 + RELTOL (default 1e-2): Relative tolerance to which the
+%                   problem should be solved.
+%                 + MAXITR (default 10000): Maximum number of iteratios
+%                   that the solver should be run for.
+%                 + rho (default 1.0): Penalty parameter for proximal
+%                   operator.
+%                 + quiet (default false): Set flag to true, to disable
+%                   output to console.
+%   factors   - Structure containing pre-computed factors. If any
+%               one field is missing, then all of them will be re-computed.
+%               The fields are:
+%                 + L, D, P: Cholesky decomposition factors of (I + A' * A) 
+%                   or (I + A * A'), whichever results in a smaller matrix.
+%                 + AA: The result of (A' * A) or (A * A'), whichever
+%                   results in the smaller dimension.
+%
+%   Outputs:
+%   x         - The solution to the optimization problem
+%   
+%   factors   - A structure of Cholesky decomposition factors.
+%               See description of corresponding input. 
+%
+% References: 
+%   http://www.stanford.edu/~boyd/papers/block_splitting.html 
+%     Block Splitting for Distributed Optimization -- N. Parikh and S. Boyd
+%
+%   http://www.stanford.edu/~boyd/papers/block_splitting.html
+%     Distributed Optimization and Statistical Learning via the Alternating
+%     Direction Method of Multipliers -- S. Boyd, N. Parikh, E. Chu,
+%     B. Peleato, and J. Eckstein
+%
+%   http://www.stanford.edu/~boyd/papers/prox_algs.html
+%     Proximal Algorithms -- N. Parikh and S. Boyd
+%
+% Authors:
+%   Neal Parikh, Chris Fougner.
+%
 
 % Parse Input.
+if nargin < 4
+  error('Not enough arguments.')
+end
 if nargin < 5
   params = [];
 end
@@ -15,23 +84,24 @@ if nargin < 6
 end
 
 ABSTOL = get_or_default(params, 'ABSTOL', 1e-4);
-RELTOL = get_or_default(params, 'RELTOL', 1e-3);
+RELTOL = get_or_default(params, 'RELTOL', 1e-2);
 MAXITR = get_or_default(params, 'MAXITR', 10000);
-rho    = get_or_default(params, 'rho', 1);
+rho    = get_or_default(params, 'rho', 1.0);
 quiet  = get_or_default(params, 'quiet', false);
 
-L = get_or_default(factors, 'L', []);
-D = get_or_default(factors, 'D', []);
-P = get_or_default(factors, 'P', []);
+L  = get_or_default(factors, 'L', []);
+D  = get_or_default(factors, 'D', []);
+P  = get_or_default(factors, 'P', []);
 AA = get_or_default(factors, 'AA', []);
 
-% Variables.
-total_time = tic;
-
+% Initialize x^k and \tilde x^k.
 [m, n] = size(A);
 x = zeros(n, 1);     xt = zeros(n, 1);
 y = zeros(m, 1);     yt = zeros(m, 1);
 z = zeros(n + m, 1); zt = zeros(n + m, 1);
+
+% Start timer.
+total_time = tic;
 
 % Precompute AAt or AtA.
 if isempty(AA) && ~issparse(A)
@@ -47,8 +117,10 @@ if ~quiet
       'eps_dual', 'objective');
 end
 
-for iter = 1:MAXITR
-  %  x^{k+1/2} = prox(x^k - \tilde x^k)
+for iter = 0:MAXITR-1
+  % Evaluate proximal operators of f and g.
+  %   y^{k+1/2} = prox(y^k - \tilde y^k)
+  %   x^{k+1/2} = prox(x^k - \tilde x^k)
   y12 = eval_prox(prox_f, y - yt, rho);
   x12 = eval_prox(prox_g, x - xt, rho);
   z12 = [x12; y12];
@@ -59,12 +131,10 @@ for iter = 1:MAXITR
     factor_time = tic;
   end
 
-  % (x^{k+1}, y^{k+1}) = Pi_A(x^{k+1/2} + \tilde x^k, y^{k+1/2} + \tilde y^k)
-  if issparse(A)
-    [z, L, D, P] = project_graph(z12 + zt, A, [], L, D, P);
-  else
-    [z, L] = project_graph(z12 + zt, A, AA, L);
-  end
+  % Project onto graph of {(x, y) \in R^{n + m} | y = Ax}, updating
+  %   (x^{k+1}, y^{k+1}) = Pi_A(x^{k+1/2} + \tilde x^k, 
+  %                             y^{k+1/2} + \tilde y^k)
+  [z, L, D, P] = project_graph(z12 + zt, A, AA, L, D, P);
 
   if iter == 1
     factor_time = toc(factor_time);
@@ -73,27 +143,31 @@ for iter = 1:MAXITR
   x = z(1:n);
   y = z(n + 1:n + m);
 
-  % Check Optimality Conditions.
+  % Check stopping criteria.
   eps_pri  = sqrt(n) * ABSTOL + RELTOL * max(norm(z12), norm(z));
   eps_dual = sqrt(n) * ABSTOL + RELTOL * norm(rho * zt);
   prires = norm(z12 - z);
   duares = rho * norm(z - zprev);
 
-  if ~quiet && (iter == 1 || mod(iter, 10) == 0)
-    obj = obj_fn(x, y);
+  converged = iter > 1 && prires < eps_pri && duares < eps_dual;
+  if ~quiet && (mod(iter, 10) == 0 || converged)
     fprintf('%4d :\t%.2e\t%.2e\t%.2e\t%.2e\t%.2e\n', ...
-        iter, prires, eps_pri, duares, eps_dual, obj);
+        iter, prires, eps_pri, duares, eps_dual, obj_fn(x, y));
   end
 
-  if iter > 2 && prires < eps_pri && duares < eps_dual
+  if converged
     break
   end
 
+  % Update dual variables.
+  %   \tilde x^{k+1} = \tilde x^{k} + x^{k+1/2} - x^k
+  %   \tilde y^{k+1} = \tilde y^{k} + y^{k+1/2} - y^k
   xt = xt + x12 - x;
   yt = yt + y12 - y;
   zt = [xt; yt];
 end
 
+% Set factors for output.
 factors.L = L;
 factors.D = D;
 factors.P = P;
@@ -108,8 +182,8 @@ end
 end
 
 function y = eval_prox(f_prox, x, rho)
-% Evaluates the proximal operator(s) on x.  f_prox may either be a 
-% function handle or a cell array of function handles
+% Evaluates the proximal operator(s) of f on x. f_prox may either be a 
+% function handle or a cell array of function handles.
 
 if iscell(f_prox)
   y = nan(size(x));
@@ -122,9 +196,13 @@ end
 
 end
 
-function varargout = project_graph(v, A, AA, L, D, P)
-% Project v onto the graph of A. 
-% Supports factorization caching and both dense/sparse A.
+function [z, L, D, P] = project_graph(v, A, AA, L, D, P)
+% Project v onto the graph of A. This is equivalent to solving
+%
+%    minimize    (1/2) ||x ? c||_2^2 + (1/2)||y ? d||_2^2
+%    subject to  y = Ax
+% 
+% Supports factorization caching and both dense and sparse A.
 
 [m, n] = size(A);
 c = v(1:n);
@@ -138,19 +216,18 @@ if issparse(A)
   end
 
   z = P * (L' \ (D \ (L \ (P' * sparse([ c + A' * d ; zeros(m, 1) ])))));
-
-  varargout(1) = {z};
-  varargout(2) = {L};
-  varargout(3) = {D};
-  varargout(4) = {P};
 else
+  % Project fat/skinny matrices onto the graph of A, by forming the normal
+  % equations, and taking Cholesky decomposition.
   if m < n
+    % Fat matrices.
     if isempty(L)
       L = chol(eye(m) + AA);
     end
     y = L \ (L' \ (A * c + AA * d));
     x = c + A' * (d - y);
   else
+    % Skinny matrices. Use matrix inversion lemma.
     if isempty(L)
       L = chol(eye(n) + AA);
     end
@@ -158,13 +235,13 @@ else
     y = A * x;
   end
 
-  varargout(1) = {[x; y]};
-  varargout(2) = {L};
+  z = [x; y];
 end
 
 end
 
 function output = get_or_default(input, var, default)
+% Returns input.var if var is a field in input, otherwise returns default.
 
 if isfield(input, var)
   output = input.(var);
